6aa326959c33dfb2c7e2020c1632b6d4b181de42
Varshitha  M A <varshnayak07@gmail.com>
2026-03-16 23:18:50 +0530

    Update ggg.java

--- ggg.java
+++ ggg.java
@@ -3,3 +3,30 @@ public class ggg{
         System.out.println("hj");
     }
 }
+package expp.expp;
+		import java.io.FileReader;
+
+		import org.json.simple.JSONObject;
+		import org.json.simple.parser.JSONParser;
+
+		public class example {
+
+		    public static void main(String[] args) throws Exception {
+
+		        JSONParser jsonparser = new JSONParser();
+
+		        FileReader filereader = new FileReader(".\\jsonfile\\exp.json");
+
+		        Object obj = jsonparser.parse(filereader);
+
+		        JSONObject empobj = (JSONObject) obj;
+
+		        String fname = (String) empobj.get("firstname");
+		        String lname = (String) empobj.get("secondname");
+
+		        System.out.println(fname);
+		        System.out.println(lname);
+		    }
+		
+
+}
